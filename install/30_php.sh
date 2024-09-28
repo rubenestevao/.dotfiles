7b83253de42b966d7276839e44dff89f936471c7
Rúben Estêvão <rubenmaster94@gmail.com>
2024-09-28 19:26:45 +0100

    updates

--- install/30_php.sh
+++
@@ -1,8 +0,0 @@
-# Install Laravel Valet
-echo 'Install laravel valet'
-echo '---------------------'
-composer global require laravel/valet
-$(composer global config home -q)/vendor/bin/valet install
-$(composer global config home -q)/vendor/bin/valet trust
-$(composer global config home -q)/vendor/bin/valet park $HOME/Sites
-
